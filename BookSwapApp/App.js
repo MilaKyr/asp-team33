05e6a75ad320db70fc25be1a2f5a0dd1d87bcaee
samie820 <samuelomole70@gmail.com>
2024-02-03 22:38:29 +0000

    setup base navigation and restyle home screen

--- BookSwapApp/App.js
+++ BookSwapApp/App.js
@@ -1,15 +1,48 @@
 import Header from './views/Header';
 import HomePage from './views/HomePage';
 import Footer from './views/Footer';
-import { SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { NativeBaseProvider, Box } from "native-base";
+
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <HomePage />
-      <Footer />
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search' : 'search-outline';
+            } else if (route.name === 'Upload') {
+              iconName = focused ? 'cloud-upload' : 'cloud-upload-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarInactiveTintColor: 'gray',
+        })}>
+          <Tab.Screen options={{
+            title: 'Book Swap'
+          }} name="Home" component={HomePage} />
+          <Tab.Screen name="Search" component={HomePage} />
+          <Tab.Screen name="Upload" component={HomePage} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 

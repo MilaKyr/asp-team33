f700b6d47b7bce5d72abd8a4c25c8b7be46a75ce
samie820 <samuelomole70@gmail.com>
2024-02-25 01:37:34 +0000

    upload new book to API

--- BookSwapApp/views/UploadBookPage.js
+++ BookSwapApp/views/UploadBookPage.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
-import { Button, CheckIcon, FormControl, Input, Select, Stack, VStack, useToast } from 'native-base';
+import { Button, CheckIcon, FormControl, Input, ScrollView, Select, Stack, VStack, useToast } from 'native-base';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { API_URL } from '../constants/api';
 
 const transformAuthors = (authors = '') => {
+    if (authors.trim().length == 0) return [];
     const splitAuthors = authors.split(',');
     return splitAuthors.map(author => {
         const [name, surname] = author.split(' ')
         return { name, surname }
     })
 }
+
+const isUndefinedOrEmpty = (field = '') => {
+    console.log(field)
+    if (Array.isArray(field) && String(field).length == 0) return true;
+    if (field == null || String(field).trim?.()?.length === 0) {
+        return true;
+    }
+};
+
 function BookUploadForm() {
     const [formData, setData] = React.useState({});
     const [courses, setCourses] = React.useState([]);
@@ -19,22 +29,23 @@ function BookUploadForm() {
     const toast = useToast();
 
 
-    const validate = () => {
-        if (formData.name === undefined) {
-            setErrors({
-                ...errors,
-                name: 'Name is required'
-            });
-            return false;
-        } else if (formData.name.length < 3) {
-            setErrors({
-                ...errors,
-                name: 'Name is too short'
-            });
-            return false;
-        }
-
-        return true;
+    const validate = (dataToSend) => {
+        console.log({ dataToSend })
+        const REQUIRED_FIELDS = [
+            'title', 'description', 'authors', 'year', 'course_id', 'book_type_id'
+        ]
+        let valid = true;
+        let errorObject = {}
+        Object.entries(dataToSend).forEach(([field, value]) => {
+            if (REQUIRED_FIELDS.includes(field) && isUndefinedOrEmpty(value)) {
+                errorObject[field] = `${field} is required`
+                valid = false;
+            }
+        })
+        setErrors(errorObject);
+        console.log({ errorObject })
+        console.log('vludate', valid)
+        return valid;
     };
 
 
@@ -43,15 +54,17 @@ function BookUploadForm() {
         const dataToSend = {
             ...formData,
             authors: transformAuthors(formData.authors),
-            book_type_id: formData.bookType,
+            book_type_id: formData.book_type_id,
             title: formData.title,
             description: formData.description,
             icbn_10: formData.icbn_10,
             year: formData.year,
             edition: formData.edition,
-            course_id: formData.course
+            course_id: formData.course_id
         }
-        console.log({dataToSend})
+        if (!validate(dataToSend)) {
+            return;
+        }
         try {
             const url = `${API_URL}/add_book`
             const response = await axios.post(url, {
@@ -93,110 +106,112 @@ function BookUploadForm() {
         getBookTypes();
     }, [])
 
-    return <VStack width="100%">
-        <FormControl isRequired isInvalid={'title' in errors}>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Title</FormControl.Label>
-            <Input size='xl' placeholder="Book Title" onChangeText={value => setData({
-                ...formData,
-                title: value
-            })} />
-            {'title' in errors ? <FormControl.ErrorMessage>Error</FormControl.ErrorMessage> : null}
-        </FormControl>
-        <FormControl isRequired isInvalid={'description' in errors}>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Description</FormControl.Label>
-            <Input size='xl' placeholder="Book details" onChangeText={value => setData({
-                ...formData,
-                description: value
-            })} />
-            {'description' in errors ? <FormControl.ErrorMessage>Error</FormControl.ErrorMessage> : null}
-        </FormControl>
-        <FormControl isRequired isInvalid={'authors' in errors}>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Authors</FormControl.Label>
-            <Input size='xl' placeholder="Authors (Comma separated e.g. John, Jane)" onChangeText={value => setData({
-                ...formData,
-                authors: value
-            })} />
-            {'authors' in errors ? <FormControl.ErrorMessage>Error</FormControl.ErrorMessage> : null}
-        </FormControl>
-        <FormControl isRequired isInvalid={'year' in errors}>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Year</FormControl.Label>
-            <Input size='xl' placeholder="Book Year" onChangeText={value => setData({
-                ...formData,
-                year: value
-            })} />
-            {'year' in errors ? <FormControl.ErrorMessage>Error</FormControl.ErrorMessage> : null}
-        </FormControl>
-
-        <FormControl isRequired isInvalid={'bookType' in errors}>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Upload type</FormControl.Label>
-            <Select selectedValue={formData.bookType} minWidth="200" accessibilityLabel="Choose Upload Type" placeholder="Choose Upload type" _selectedItem={{
-                bg: "teal.600",
-                endIcon: <CheckIcon size="5" />
-            }} mt={1} onValueChange={itemValue => {
-                setData({
+    return <ScrollView>
+        <VStack width="100%">
+            <FormControl isRequired isInvalid={'title' in errors}>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Title</FormControl.Label>
+                <Input size='xl' placeholder="Book Title" onChangeText={value => setData({
                     ...formData,
-                    bookType: itemValue
-                })
-            }}>
-                {bookTypes.map(bookType => (
-                    <Select.Item label={bookType.name} value={bookType.id} />
-                ))}
-            </Select>
-            {'bookType' in errors ? <FormControl.ErrorMessage>Error</FormControl.ErrorMessage> : null}
-        </FormControl>
-
-        <FormControl isRequired isInvalid={'course' in errors}>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Course</FormControl.Label>
-            <Select selectedValue={formData.course} minWidth="200" accessibilityLabel="Choose a course" placeholder="Choose Upload type" _selectedItem={{
-                bg: "teal.600",
-                endIcon: <CheckIcon size="5" />
-            }} mt={1} onValueChange={itemValue => {
-                setData({
+                    title: value
+                })} />
+                {'title' in errors ? <FormControl.ErrorMessage>{errors.title}</FormControl.ErrorMessage> : null}
+            </FormControl>
+            <FormControl isRequired isInvalid={'description' in errors}>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Description</FormControl.Label>
+                <Input size='xl' placeholder="Book details" onChangeText={value => setData({
                     ...formData,
-                    course: itemValue
-                })
-            }}>
-                {courses.map(course => (
-                    <Select.Item label={course.name} value={course.id} />
-                ))}
-            </Select>
-            {'course' in errors ? <FormControl.ErrorMessage>Error</FormControl.ErrorMessage> : null}
-        </FormControl>
+                    description: value
+                })} />
+                {'description' in errors ? <FormControl.ErrorMessage>{errors.description}</FormControl.ErrorMessage> : null}
+            </FormControl>
+            <FormControl isRequired isInvalid={'authors' in errors}>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Authors</FormControl.Label>
+                <Input size='xl' placeholder="Authors (Comma separated e.g. John, Jane)" onChangeText={value => setData({
+                    ...formData,
+                    authors: value
+                })} />
+                {'authors' in errors ? <FormControl.ErrorMessage>{errors.authors}</FormControl.ErrorMessage> : null}
+            </FormControl>
+            <FormControl isRequired isInvalid={'year' in errors}>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Year</FormControl.Label>
+                <Input size='xl' placeholder="Book Year" onChangeText={value => setData({
+                    ...formData,
+                    year: value
+                })} />
+                {'year' in errors ? <FormControl.ErrorMessage>{errors.year}</FormControl.ErrorMessage> : null}
+            </FormControl>
 
-        <FormControl>
-            <FormControl.Label _text={{
-                bold: true
-            }}>ICBN</FormControl.Label>
-            <Input size='xl' placeholder="Book Year" onChangeText={value => setData({
-                ...formData,
-                icbn_10: value
-            })} />
-        </FormControl>
-        <FormControl>
-            <FormControl.Label _text={{
-                bold: true
-            }}>Book Edition</FormControl.Label>
-            <Input size='xl' placeholder="Edition (optional)" onChangeText={value => setData({
-                ...formData,
-                edition: value
-            })} />
-        </FormControl>
-        <Button size='lg' onPress={onSubmit} mt="5" colorScheme="cyan">
-            Upload Book
-        </Button>
-    </VStack>;
+            <FormControl isRequired isInvalid={'book_type_id' in errors}>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Upload type</FormControl.Label>
+                <Select selectedValue={formData.book_type_id} minWidth="200" accessibilityLabel="Choose Upload Type" placeholder="Choose Upload type" _selectedItem={{
+                    bg: "teal.600",
+                    endIcon: <CheckIcon size="5" />
+                }} mt={1} onValueChange={itemValue => {
+                    setData({
+                        ...formData,
+                        book_type_id: itemValue
+                    })
+                }}>
+                    {bookTypes.map(bookType => (
+                        <Select.Item label={bookType.name} value={bookType.id} />
+                    ))}
+                </Select>
+                {'book_type_id' in errors ? <FormControl.ErrorMessage>{errors.book_type_id}</FormControl.ErrorMessage> : null}
+            </FormControl>
+
+            <FormControl isRequired isInvalid={'course_id' in errors}>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Course</FormControl.Label>
+                <Select selectedValue={formData.course_id} minWidth="200" accessibilityLabel="Choose a course" placeholder="Choose Upload type" _selectedItem={{
+                    bg: "teal.600",
+                    endIcon: <CheckIcon size="5" />
+                }} mt={1} onValueChange={itemValue => {
+                    setData({
+                        ...formData,
+                        course_id: itemValue
+                    })
+                }}>
+                    {courses.map(course => (
+                        <Select.Item label={course.name} value={course.id} />
+                    ))}
+                </Select>
+                {'course_id' in errors ? <FormControl.ErrorMessage>{errors.course_id}</FormControl.ErrorMessage> : null}
+            </FormControl>
+
+            <FormControl>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>ICBN</FormControl.Label>
+                <Input size='xl' placeholder="Book Year" onChangeText={value => setData({
+                    ...formData,
+                    icbn_10: value
+                })} />
+            </FormControl>
+            <FormControl>
+                <FormControl.Label _text={{
+                    bold: true
+                }}>Book Edition</FormControl.Label>
+                <Input size='xl' placeholder="Edition (optional)" onChangeText={value => setData({
+                    ...formData,
+                    edition: value
+                })} />
+            </FormControl>
+            <Button size='lg' onPress={onSubmit} mt="5" colorScheme="cyan">
+                Upload Book
+            </Button>
+        </VStack>
+    </ScrollView>;
 }
 
 const UploadBookPage = () => {
